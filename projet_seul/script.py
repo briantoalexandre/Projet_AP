8ee1bb2f30e475fd314fd1639a9af5edf539923e
alexa <alexandre.brianto29@gmail.com>
2026-02-22 19:33:07 +0100

    python

--- projet_seul/script.py
+++ projet_seul/script.py
@@ -1,11 +1,28 @@
 from json import load
+from sys import argv
+from os import path
 
-filename = "userProfile.json"
+directory = (lambda tup=path.split(argv[0]): tup[0]+"/" if bool(tup[0]) else "./")
+filename = (directory()+"userProfile.json").replace("\\", "/")
+
+print(filename)
 def read():
-    try:
-        with open(filename, "r") as rf:
-            print(*load(rf), sep="\n")
-    except IndexError:
-        print("error")
+    with open(filename, "r") as rf:
+        print(*load(rf), sep="\n")
 
-read()
+def flush():
+    with open(filename, "w") as wf:
+        wf.write("[]")
+
+def main():
+    args = argv
+    try:
+        if len(args) == 2 and args[1].lower() == "flush":
+            flush()
+        else:
+            read()
+    except Exception as e:
+        print(f"ERROR : {e}")
+
+main()
+    
