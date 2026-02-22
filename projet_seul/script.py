007c05878e56e68a8633ce814c246665f2f19fa1
alexa <alexandre.brianto29@gmail.com>
2026-02-22 19:38:27 +0100

    python again

--- projet_seul/script.py
+++ projet_seul/script.py
@@ -2,19 +2,24 @@ from json import load
 from sys import argv
 from os import path
 
+# argv from sys allows to take arguments from the command line like : 'python3 script.py flush'
+
 directory = (lambda tup=path.split(argv[0]): tup[0]+"/" if bool(tup[0]) else "./")
 filename = (directory()+"userProfile.json").replace("\\", "/")
 
-print(filename)
 def read():
+    """print the content of the file"""
     with open(filename, "r") as rf:
         print(*load(rf), sep="\n")
 
 def flush():
+    """clean the file"""
     with open(filename, "w") as wf:
         wf.write("[]")
+    print(f"{filename} flushed!")
 
 def main():
+    """main function"""
     args = argv
     try:
         if len(args) == 2 and args[1].lower() == "flush":
@@ -25,4 +30,5 @@ def main():
         print(f"ERROR : {e}")
 
 main()
+print("END OF FILE\n")
     
