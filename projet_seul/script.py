8a65467215a9fe37b3b1dc4da24f201760fb17ff
alexa <alexandre.brianto29@gmail.com>
2026-02-22 18:56:09 +0100

    py phase

--- projet_seul/script.py
+++ projet_seul/script.py
@@ -4,7 +4,7 @@ filename = "userProfile.json"
 def read():
     try:
         with open(filename, "r") as rf:
-            print([f"{key} : {value}" for key, value in load(rf)], sep="\n")
+            print(*load(rf), sep="\n")
     except IndexError:
         print("error")
 
