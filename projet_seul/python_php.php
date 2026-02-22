1d7513ab7a45b735383ef915097d199d433c135e
alexa <alexandre.brianto29@gmail.com>
2026-02-22 17:43:35 +0100

    no more

--- projet_seul/python_php.php
+++ projet_seul/python_php.php
@@ -12,25 +12,8 @@ if (isset($username) and !empty($username)) {
     if (isset($passwd) and !empty($passwd)) {
         if (isset($email) and !empty($email)) {
             if (isset($name) and !empty($name)) {
-                $append = file_get_contents($filename);
-                echo("value : '$append'<br><br>");
-                
-                
-                if (strlen($append[0]) === 0) {
-                    $append = array();
-                    $id = 1;
-                    echo("1");
-                }
-                else {
-                    echo("2");
-                }
-                echo "<br><br>";
-                $append = json_decode($append);
-                foreach($append as $value=>$val) {
-                   print_r($value);
-                   echo " => $val<br>";
-                }
-                echo "<br><br>";
+                $append = json_decode(file_get_contents($filename));
+                print_r($append);
                 
 
                 if (1==2) {
