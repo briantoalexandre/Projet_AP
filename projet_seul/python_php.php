78ff946ccf7a09bb677918e396cb81b56ee41362
alexa <alexandre.brianto29@gmail.com>
2026-02-22 17:01:51 +0100

    changed

--- projet_seul/python_php.php
+++ projet_seul/python_php.php
@@ -14,7 +14,8 @@ if (isset($username) and !empty($username)) {
             if (isset($name) and !empty($name)) {
                 $append = array(file_get_contents($filename));
                 print_r($append);
-                if (empty($append)) {
+                if (count($append)<=1) {
+                    $append = array()
                     $id = 1;
                     echo("1");
                 }
